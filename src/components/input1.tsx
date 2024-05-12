afa088a87b258c9904873294d3a9aa36c1cee365
phiniqq724 <fahrellnew@gmail.com>
2024-05-12 12:25:59 +0700

    s

--- src/components/input1.tsx
+++ src/components/input1.tsx
@@ -14,7 +14,13 @@ export const Input = ({ InText, InType, InName, onChange, value }: CInput) => {
     <label htmlFor="">
       <span className="text-gray-500 sm:text-2xl xl:text-xl">{InText} :</span>
       <br />
-      <input type={InType} onChange={onChange} value={value} className="xl:h-[56px] sm:h-[70px] rounded-xl xl:w-[478px] sm:w-[675px] bg-gray-100 text-justify px-4" name={InName} />
+      <input
+        type={InType}
+        onChange={onChange}
+        value={value}
+        className="xl:h-[56px] sm:h-[70px] rounded-xl xl:w-[478px] sm:w-[675px] bg-gray-100 text-justify px-4"
+        name={InName}
+      />
     </label>
   );
 };
@@ -24,7 +30,13 @@ export const Input2 = ({ InText, InType, InName, onChange, value }: CInput) => {
     <label htmlFor="">
       <span className="text-gray-500 xl:text-xl sm:text-2xl">{InText} :</span>
       <br />
-      <input type={InType} onChange={onChange} value={value} className="xlh-[56px] rounded-xl sm:h-[70px] xl:w-[220px] sm:w-[675px] bg-gray-100 px-4" name={InName} />
+      <input
+        type={InType}
+        onChange={onChange}
+        value={value}
+        className="xlh-[56px] rounded-xl sm:h-[70px] xl:w-[220px] sm:w-[675px] bg-gray-100 px-4"
+        name={InName}
+      />
     </label>
   );
 };
@@ -34,7 +46,14 @@ export const Input3 = ({ InText, InName, onChange, value }: CInput) => {
     <label htmlFor="">
       <span className="text-gray-500 sm:text-2xl xl:text-xl">{InText} :</span>
       <br />
-      <textarea onChange={() => onChange} value={value} className="h-[260px] rounded-xl xl:w-[1040px] sm:w-[675px] bg-gray-100 p-4" rows={4} cols={50} name={InName}></textarea>
+      <textarea
+        onChange={() => onChange}
+        value={value}
+        className="h-[260px] rounded-xl xl:w-[1040px] sm:w-[675px] bg-gray-100 p-4"
+        rows={4}
+        cols={50}
+        name={InName}
+      ></textarea>
     </label>
   );
 };
