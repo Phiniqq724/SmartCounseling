694ae9ee9d8a7553f14b67da2ddf177b03fb2bb5
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-05-12 15:03:20 +0700

    ketinggalan

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,17 +14,10 @@ export default function Component() {
           <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
-          <Navbar.Link
-            href="#"
-            active
-            className="hover:text-oceanb text-deepb bg-transparent no-underline hover:underline hover:underline-offset-1 transition-transform duration-200 transform hover:scale-105 text-xl"
-          >
+          <Navbar.Link href="#" active className="hover:text-oceanb text-deepb bg-transparent no-underline hover:underline hover:underline-offset-1 transition-transform duration-200 transform hover:scale-105 text-xl">
             Home
           </Navbar.Link>
-          <Navbar.Link
-            className="hover:text-oceanb text-deepb no-underline hover:underline hover:underline-offset-1 transition-transform duration-200 transform hover:scale-105 text-xl"
-            href="#Topics"
-          >
+          <Navbar.Link className="hover:text-oceanb text-deepb no-underline hover:underline hover:underline-offset-1 transition-transform duration-200 transform hover:scale-105 text-xl" href="#Topics">
             Topics
           </Navbar.Link>
         </Navbar.Collapse>
