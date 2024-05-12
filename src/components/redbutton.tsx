57226a7128e5220a794c7ae53db8911a4daad368
phiniqq724 <fahrellnew@gmail.com>
2024-05-12 11:30:06 +0700

    chore: Update RedButton component to include BtnType prop

--- src/components/redbutton.tsx
+++ src/components/redbutton.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 
-export default function RedButton({ BtnText, Navigate }: any) {
+export default function RedButton({ BtnText, Navigate, BtnType }: any) {
   return (
     <Link href={Navigate}>
-      <button className="bg-redbutton text-white rounded-xl text-[22px] xl:h-[56px] xl:w-[236px] sm:w-[150px] sm:h-[64px]">
+      <button
+        className="bg-redbutton text-white rounded-xl text-[22px] xl:h-[56px] xl:w-[236px] sm:w-[150px] sm:h-[64px]"
+        type={BtnType}
+      >
         {BtnText}
       </button>
     </Link>
